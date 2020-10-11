411d8e427b813ea5bfb69bd9adc2976cfbc73235
Ronald Stewart <rstewart456@gmail.com>
2020-10-11 17:37:11 -0400

    Updates

---
+++ precache-manifest.f6766e8bb80af55531fcd75301f4d6fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bedd088eccfde6d527fc250bd3383f5",
+    "url": "/react-portfoliov3/index.html"
+  },
+  {
+    "revision": "cc040492ca48cdd34b16",
+    "url": "/react-portfoliov3/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ff00b825bbd9e548921a",
+    "url": "/react-portfoliov3/static/css/main.38d37c0b.chunk.css"
+  },
+  {
+    "revision": "cc040492ca48cdd34b16",
+    "url": "/react-portfoliov3/static/js/2.61e61741.chunk.js"
+  },
+  {
+    "revision": "659df5b420806b55437f4f9e57f60411",
+    "url": "/react-portfoliov3/static/js/2.61e61741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff00b825bbd9e548921a",
+    "url": "/react-portfoliov3/static/js/main.c43075b6.chunk.js"
+  },
+  {
+    "revision": "f5a8c72b00b807ed8ded",
+    "url": "/react-portfoliov3/static/js/runtime-main.7449e066.js"
+  }
+]);
